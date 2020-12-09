c025e2ea308132a3970291096db122467ee2dafc
2B键盘 <2968640488@qq.com>
2020-12-09 23:13:40 +0800

    feat(build.gohan): 新增一些构建方法

--- gohan-builder/src/main/java/com/dahan/gohan/buildscript/GohanBuilder.groovy
+++ gohan-builder/src/main/java/com/dahan/gohan/buildscript/GohanBuilder.groovy
@@ -1,5 +1,8 @@
 package com.dahan.gohan.buildscript
 
+import com.dahan.gohan.repository.GohanDependency
+import com.dahan.gohan.task.GohanTask
+
 /* ************************************************************************
  *
  * Copyright (C) 2020 2B键盘 All rights reserved.
@@ -28,11 +31,6 @@ package com.dahan.gohan.buildscript
 class GohanBuilder
 {
 
-    /**
-     * 模块信息
-     */
-    private ProjectInformation projectInformation
-
     private static final def GROUP = 0
 
     private static final def ARTIFACT = 1
@@ -40,47 +38,89 @@ class GohanBuilder
     private static final def VERSION = 2
 
     /**
+     * 项目中依赖的Jar包
+     */
+    List<GohanDependency> gohanDependencies = []
+
+    /**
+     * 模块信息
+     */
+    def projectInformation = new ProjectInformation()
+
+    /**
+     * 属性，相当于pom中的properties
+     */
+    Map<String, String> exts = new HashMap<>()
+
+    /**
+     * 属性
+     */
+    Map<String, String> exts(Closure closure) {
+        return this.exts
+    }
+
+    /**
      * 模块groupId
      */
-    def groupId(String group) { projectInformationNotNull(group, GROUP) }
+    void groupId(String group) { projectInformationNotNull(group, GROUP) }
 
     /**
      * 模块artifactId
      */
-    def artifactId(String artifact) { projectInformationNotNull(artifact, ARTIFACT) }
+    void artifactId(String artifact) { projectInformationNotNull(artifact, ARTIFACT) }
 
     /**
      * 模块当前版本号
      */
-    def version(String version) { projectInformationNotNull(version, VERSION) }
+    void version(String version) { projectInformationNotNull(version, VERSION) }
 
     /**
      * 仓库节点
      */
-    def repository(Closure closure) {};
+    void repositories(Closure closure) { closure.call() }
 
     /**
      * 添加仓库信息
      */
-    def maven(String name, String address){}
+    void repository(String url) {}
 
     /**
      * 模块引用的依赖包
      */
-    void optionals(Closure closure) { closure.call() }
+    void dependencies(Closure closure)
+    {
+        closure.call()
+    }
 
     /**
      * 使用插件
      * @param name 插件名称
      */
-    def plugin(String name) {}
+    void plugin(String name) {}
 
     /**
      * 依赖信息，主要信息为groupId, artifactId, version。其他的为自定义属性。
      * 例如pom.xml中dependency节点下的scpoe、type、compile等字段。
      */
-    def optional(LinkedHashMap<String, String> settings) {
+    void dependency(LinkedHashMap<String, String> settings)
+    {
+        gohanDependencies.add(new GohanDependency(
+                settings.group,
+                settings.name,
+                settings.version,
+                settings.classifier,
+                settings.scope))
     }
+
+    /**
+     * 自定义任务
+     */
+    GohanTask task(String name, Closure closure) { closure.call() }
+
+    /**
+     * 任务定义方法块
+     */
+    void tasks (Closure closure) { closure.call() }
 
     /**
      * 添加模块信息
@@ -88,11 +128,14 @@ class GohanBuilder
      * @param value 属性值
      * @param type 属性类型
      */
-    void projectInformationNotNull(String value, int type) {
-        if (projectInformation) {
+    void projectInformationNotNull(String value, int type)
+    {
+        if (projectInformation)
+        {
             projectInformation = new ProjectInformation()
         }
-        switch (type) {
+        switch (type)
+        {
             case GROUP: {
                 projectInformation.setGroupId(value); break
             }
